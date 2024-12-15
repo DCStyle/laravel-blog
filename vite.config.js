435e854b3831531dd289eac2884aa64aa84efefa
dcstylexf <dcstylexf@gmail.com>
2024-12-16 01:49:01 +0700

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { glob } from 'glob';
+import fs from 'fs';
+import path from 'path';
+
+function getFiles(dir, ext) {
+    let files = [];
+    const items = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const item of items) {
+        if (item.isDirectory()) {
+            files = [...files, ...getFiles(path.join(dir, item.name), ext)];
+        } else if (item.name.endsWith(ext)) {
+            files.push(path.join(dir, item.name));
+        }
+    }
+
+    return files;
+}
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                ...glob.sync('resources/css/**/*.css'),
-                ...glob.sync('resources/js/**/*.js'),
-                ...glob.sync('resources/scss/**/*.scss'),  // If you have SCSS files
+                ...getFiles('resources/css', '.css'),
+                ...getFiles('resources/js', '.js'),
+                ...getFiles('resources/scss', '.scss'),
             ],
             refresh: true,
         }),
